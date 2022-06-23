c3c5e1dd6304215ea5995685d8c13c9f2b85e9e6
Sahana Reddy <sahanacmreddy@gmail.com>
2022-06-23 22:20:02 +0530

    Interceptors

--- src/app/authh.interceptor.ts
+++ src/app/authh.interceptor.ts
@@ -3,17 +3,36 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
 
 @Injectable()
 export class AuthhInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // console.log(request)
-    return next.handle(request)
+    // console.log(Response)
+    return next.handle(request).pipe(
+      tap((event: any) => {
+        if (event instanceof HttpResponse) {
+          // http response status code
+          console.log("status Code")
+          console.log(event.status);
+        }
+      }, error => {
+        // http response status code
+        console.log("----response----");
+        console.error("status code:");
+        console.error(error.status);
+        console.error(error.message);
+        console.log("--- end of response---");
+
+      })
+    )
   }
 }
